52837d2f9c3297917cec8f4b681d8004f319a6ec
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2019-12-03 12:20:59 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,117 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+// element 요소로 픽셀값을 지정해줘야 그릴 수 있다.
+canvas.width=CANVAS_SIZE;
+canvas.height=CANVAS_SIZE;
+
+ctx.fillStyle = "white"; //background color를 투명하고 싶으면 설정 지우기
+ctx.fillRect(0,0,CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting(){
+    painting = false;
+}
+ 
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+/*
+function onMouseDown(event){
+   painting = ture;    
+}
+
+function onMouseUp(event){
+    stopPainting();
+}*/
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "FILL";
+    } else {
+        filling = true;
+        mode.innerText = "PAINT";
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL(); //("image/jpeg") jpeg로 저장
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting /*onMoustDown*/);
+    canvas.addEventListener("mouseup", stopPainting /*onMouseUp*/);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM); //contextmenu : 우클릭 방지
+}
+
+Array.from(colors).forEach(
+    // color는 array안에 들어가는 각각의 아이템 대표로 이름은 무의미하다
+    // 배열된 각 컬러를 클릭하면 handleColorClick을 반환하라
+    color => color.addEventListener("click", handleColorClick)
+);
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
